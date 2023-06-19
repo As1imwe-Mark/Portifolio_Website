3cd8a045eaa33913ff7d456317a01812c0c5fb33
Asiimwe Mark <100065970+As1imwe-Mark@users.noreply.github.com>
2023-06-20 01:44:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const menu = document.querySelector("#mobile")
-const openBtn = document.querySelector("#menu-icon")
-const closeBtn = document.querySelector(".menu_close")
-const items = document.querySelectorAll("#mobile ul li")
+const menu = document.querySelector('#mobile');
+const openBtn = document.querySelector('#menu-icon');
+const closeBtn = document.querySelector('.menu_close');
+const items = document.querySelectorAll('#mobile ul li');
 openBtn.addEventListener('click', () => {
-  menu.classList.add("mobile")
+  menu.classList.add("mobile");
 })
 closeBtn.addEventListener('click', () => {
-  menu.classList.remove('mobile')
-})
+  menu.classList.remove('mobile');
+});
 items.forEach((e) => {
   e.addEventListener('click', () => {
-    menu.classList.remove('mobile')
+    menu.classList.remove('mobile');
   })
-})
+});
