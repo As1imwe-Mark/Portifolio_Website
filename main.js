040632027893827ac8710fe894f4b6967754a352
Asiimwe Mark <100065970+As1imwe-Mark@users.noreply.github.com>
2023-06-20 01:46:44 +0300

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ const openBtn = document.querySelector('#menu-icon');
 const closeBtn = document.querySelector('.menu_close');
 const items = document.querySelectorAll('#mobile ul li');
 openBtn.addEventListener('click', () => {
-  menu.classList.add("mobile");
-})
+  menu.classList.add('mobile');
+});
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('mobile');
 });
 items.forEach((e) => {
   e.addEventListener('click', () => {
     menu.classList.remove('mobile');
-  })
+  });
 });
